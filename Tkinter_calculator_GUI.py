b2843140d2db9ca6068a79cd76a0e3b05d702734
Deepak PG <123804790+Deep4kpg@users.noreply.github.com>
2023-02-04 20:33:58 +0530

    Update Tkinter_calculator_GUI.py

--- Tkinter_calculator_GUI.py
+++ Tkinter_calculator_GUI.py
@@ -1,5 +1,4 @@
-# Python program to create a simple GUI
-# calculator using Tkinter
+# Python program to create a simple GUI calculator using Tkinter
 
 # import everything from tkinter module
 from tkinter import *
@@ -8,8 +7,6 @@ from tkinter import *
 expression = ""
 
 
-# Function to update expression
-# in the text entry box
 def press(num):
 	# point out the global expression variable
 	global expression
@@ -21,39 +18,27 @@ def press(num):
 	equation.set(expression)
 
 
-# Function to evaluate the final expression
-def equalpress():
-	# Try and except statement is used
-	# for handling the errors like zero
-	# division error etc.
 
-	# Put that code inside the try block
-	# which may generate the error
+def equalpress():
+	
 	try:
 
 		global expression
 
-		# eval function evaluate the expression
-		# and str function convert the result
-		# into string
 		total = str(eval(expression))
 
 		equation.set(total)
 
-		# initialize the expression variable
-		# by empty string
 		expression = ""
 
-	# if error is generate then handle
-	# by the except block
+	# if error is generate then handle by the except block
 	except:
 
 		equation.set(" error ")
 		expression = ""
 
 
-# Function to clear the contents
-# of text entry box
+# Function to clear the contents of text entry box
 def clear():
 	global expression
 	expression = ""
@@ -74,23 +59,16 @@ if __name__ == "__main__":
 	# set the configuration of GUI window
 	gui.geometry("270x150")
 
-	# StringVar() is the variable class
-	# we create an instance of this class
+
 	equation = StringVar()
 
-	# create the text entry box for
-	# showing the expression .
+	# create the text entry box for showing the expression
 	expression_field = Entry(gui, textvariable=equation)
 
-	# grid method is used for placing
-	# the widgets at respective positions
-	# in table like structure .
+	
 	expression_field.grid(columnspan=4, ipadx=70)
 
-	# create a Buttons and place at a particular
-	# location inside the root window .
-	# when user press the button, the command or
-	# function affiliated to that button is executed .
+	# create buttons
 	button1 = Button(gui, text=' 1 ', fg='black', bg='red',
 					command=lambda: press(1), height=1, width=7)
 	button1.grid(row=2, column=0)
